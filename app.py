89c669832e67fe22033a56d4fdda8bb0bb46e847
andrerclaudio <andre.ribeiro.srs@gmail.com>
2020-01-16 16:22:04 -0300

    Improvements

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-import itertools
-import pandas as pd
 from turing.file import file_upload
+import stackoverflow
+import hetland
+import wikipedia
 
 
 def application():
@@ -12,9 +13,8 @@ def application():
         if content is False or reward == 0:
             break
 
-        df = pd.DataFrame(columns=['Valores', 'Soma'])
-        values = []
         number = ''
+        values = []
 
         for character in content:
             if character == '\n':
@@ -26,33 +26,12 @@ def application():
         if number != '':
             values.append(int(number))
 
-        sun = 0
-        success = False
+        break
 
-        for i in range(0, len(values) + 1):
-            for subset in itertools.combinations(values, i):
-                high_value = True
-                for j in subset:
-                    sun = sun + j
-                    if sun > reward:
-                        high_value = False
-                        sun = 0
-                        break
-                    elif sun == reward:
-                        success = True
-                        break
+    so = stackoverflow.stackoverflow(values, reward)
+    he = hetland.bb_knapsack(values, reward)
+    wi = wikipedia.approx_with_accounting_and_duplicates(values, reward)
 
-                if high_value:
-                    df = df.append({'Valores': subset, 'Soma': sun}, ignore_index=True)
-                    sun = 0
-
-                if success:
-                    break
-
-            if success:
-                break
-
-        df.sort_values(by=['Soma'], ascending=False, inplace=True)
-        print(reward)
-        print(df)
-
+    print('Stackoverflow:', so)
+    print('Hetland:', he)
+    print('Wikipedia:', wi)
